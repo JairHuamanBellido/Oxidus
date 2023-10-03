126353a1bcb62c954e1108592873e103a2570572
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2023-10-03 18:36:36 -0500

    Add slider chadcn component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { Slider } from "@/components/ui/slider";
+
 export default function Home() {
   return (
     <main className="min-h-screen p-24">
       <h1>Hello world</h1>
+
+      <Slider defaultValue={[128]} step={1} max={256} />
     </main>
   );
 }

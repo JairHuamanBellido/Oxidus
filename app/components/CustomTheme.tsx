d196385f68fb9182f473aad812cdb4a94a0c96b7
Jair Huaman Bellido <huamanbellidoj@gmail.com>
2023-10-11 10:48:09 -0500

    Refactor/manage css variable in context (#16)
    * refactor(theme): manage shadcn css variables in context
    
    * refactor(theme): update layout and slider style

--- app/components/CustomTheme.tsx
+++ app/components/CustomTheme.tsx
@@ -1,25 +1,21 @@
 import { useThemeContext } from "@/components/contexts/ThemeContext/ThemeContext";
-import Color from "color";
 import { useEffect, useRef } from "react";
 import ComponentsShowCase from "./ComponentsShowCase";
 
-import {
-  oneLight,
-  oneDark,
-} from "react-syntax-highlighter/dist/esm/styles/prism";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
 
-function extractHSLValues(color: string) {
-  const hue = {
-    h: Color(color).hsl().hue().toFixed(2).toString(),
-    s: Color(color).hsl().saturationl().toFixed(2).toString(),
-    l: Color(color).hsl().lightness().toFixed(2).toString(),
-  };
+import { extractHSLValues } from "@/src/utils/getHSLValues";
+import { generateShadcnCssVariables } from "@/src/utils/generateShadcnCssVariables";
+import DefaultTheme from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-  return `${hue.h} ${hue.s}% ${hue.l}%`;
-}
+SyntaxHighlighter.supportedLanguages;
 export default function CustomTheme() {
-  const { hex, darkColors, lightColors } = useThemeContext();
+  const {
+    cssVariables: {
+      shadcn: { dark, light },
+    },
+    hex,
+  } = useThemeContext();
   const lightRef = useRef<HTMLDivElement>(null);
   const darkRef = useRef<HTMLDivElement>(null);
 
@@ -30,267 +26,179 @@ export default function CustomTheme() {
 
     lightRef.current?.style.setProperty(
       "--background",
-      extractHSLValues(lightColors[lightColors.length - 1]),
+      extractHSLValues(light.background),
     );
     lightRef.current?.style.setProperty(
       "--foreground",
-      extractHSLValues(Color(hex).mix(Color("black"), 0.9).hsl().string()),
+      extractHSLValues(light.foreground),
     );
 
-    lightRef.current?.style.setProperty("--primary", extractHSLValues(hex));
+    lightRef.current?.style.setProperty(
+      "--primary",
+      extractHSLValues(light.primary),
+    );
     lightRef.current?.style.setProperty(
       "--primary-foreground",
-      extractHSLValues(Color("#ffffff").hex()),
+      extractHSLValues(light.primaryForeground),
     );
 
-    lightRef.current?.style.setProperty(
-      "--card",
-      extractHSLValues(lightColors[lightColors.length - 1]),
-    );
+    lightRef.current?.style.setProperty("--card", extractHSLValues(light.card));
 
     lightRef.current?.style.setProperty(
       "--card-foreground",
-      extractHSLValues(Color(hex).mix(Color("black"), 0.9).hsl().string()),
+      extractHSLValues(light.cardForeground),
     );
 
     lightRef.current?.style.setProperty(
       "--popover",
-      extractHSLValues(Color("#ffffff").hsl().hex()),
+      extractHSLValues(light.popover),
     );
     lightRef.current?.style.setProperty(
       "--popover-foreground",
-      extractHSLValues(Color(hex).mix(Color("black"), 0.9).hsl().string()),
+      extractHSLValues(light.popoverForeground),
     );
 
     lightRef.current?.style.setProperty(
       "--secondary",
-      extractHSLValues(Color(hex).mix(Color("white"), 0.85).rgb().string()),
+      extractHSLValues(light.secondary),
     );
 
     lightRef.current?.style.setProperty(
       "--secondary-foreground",
-      extractHSLValues(darkColors[0]),
+      extractHSLValues(light.secondaryForeground),
     );
 
     lightRef.current?.style.setProperty(
       "--muted",
-      extractHSLValues(Color(hex).mix(Color("white"), 0.95).rgb().string()),
+      extractHSLValues(light.muted),
     );
     lightRef.current?.style.setProperty(
       "--muted-foreground",
-      extractHSLValues(Color("#ffffff").hsl().darken(0.5).hex()),
+      extractHSLValues(light.mutedForeground),
     );
 
     lightRef.current?.style.setProperty(
       "--accent",
-      extractHSLValues(Color(hex).mix(Color("white"), 0.95).rgb().string()),
+      extractHSLValues(light.accent),
     );
 
     lightRef.current?.style.setProperty(
       "--accent-foreground",
-      extractHSLValues(hex),
+      extractHSLValues(light.accentForeground),
     );
 
     lightRef.current?.style.setProperty(
       "--border",
-      extractHSLValues(Color("#ffffff").hsl().darken(0.1).hex()),
+      extractHSLValues(light.border),
     );
     lightRef.current?.style.setProperty(
       "--input",
-      extractHSLValues(Color("#ffffff").hsl().darken(0.1).hex()),
+      extractHSLValues(light.input),
     );
-    lightRef.current?.style.setProperty("--ring", extractHSLValues(hex));
+    lightRef.current?.style.setProperty("--ring", extractHSLValues(light.ring));
 
     /**Dark settings */
-
     darkRef.current?.style.setProperty(
       "--background",
-      extractHSLValues(darkColors[0]),
+      extractHSLValues(dark.background),
     );
     darkRef.current?.style.setProperty(
       "--foreground",
-      extractHSLValues(Color(hex).mix(Color("white"), 0.85).hsl().string()),
+      extractHSLValues(dark.foreground),
     );
 
-    darkRef.current?.style.setProperty("--primary", extractHSLValues(hex));
+    darkRef.current?.style.setProperty(
+      "--primary",
+      extractHSLValues(dark.primary),
+    );
     darkRef.current?.style.setProperty(
       "--primary-foreground",
-      extractHSLValues(Color("#ffffff").hex()),
+      extractHSLValues(dark.primaryForeground),
     );
 
-    darkRef.current?.style.setProperty(
-      "--card",
-      extractHSLValues(darkColors[0]),
-    );
+    darkRef.current?.style.setProperty("--card", extractHSLValues(dark.card));
 
     darkRef.current?.style.setProperty(
       "--card-foreground",
-      extractHSLValues(Color(hex).mix(Color("white"), 0.85).hsl().string()),
+      extractHSLValues(dark.cardForeground),
     );
 
     darkRef.current?.style.setProperty(
       "--popover",
-      extractHSLValues(Color(darkColors[0]).hsl().hex()),
+      extractHSLValues(dark.popover),
     );
     darkRef.current?.style.setProperty(
       "--popover-foreground",
-      extractHSLValues(Color(hex).mix(Color("white"), 0.85).hsl().string()),
+      extractHSLValues(dark.popoverForeground),
     );
 
     darkRef.current?.style.setProperty(
       "--secondary",
-      extractHSLValues(Color("black").mix(Color(hex), 0.35).rgb().string()),
+      extractHSLValues(dark.secondary),
     );
 
     darkRef.current?.style.setProperty(
       "--secondary-foreground",
-      extractHSLValues(Color("#ffffff").hex()),
+      extractHSLValues(dark.secondaryForeground),
     );
 
-    darkRef.current?.style.setProperty(
-      "--muted",
-      extractHSLValues(
-        Color(darkColors[0]).mix(Color("white"), 0.1).rgb().string(),
-      ),
-    );
+    darkRef.current?.style.setProperty("--muted", extractHSLValues(dark.muted));
     darkRef.current?.style.setProperty(
       "--muted-foreground",
-      extractHSLValues(
-        Color(darkColors[0]).mix(Color("white"), 0.3).rgb().string(),
-      ),
+      extractHSLValues(dark.mutedForeground),
     );
 
     darkRef.current?.style.setProperty(
       "--accent",
-      extractHSLValues(Color("black").mix(Color(hex), 0.35).rgb().string()),
+      extractHSLValues(dark.accent),
     );
 
     darkRef.current?.style.setProperty(
       "--accent-foreground",
-      extractHSLValues(Color(hex).mix(Color("white"), 0.85).hsl().string()),
+      extractHSLValues(dark.accentForeground),
     );
 
     darkRef.current?.style.setProperty(
       "--border",
-      extractHSLValues(Color("black").mix(Color("white"), 0.15).rgb().string()),
+      extractHSLValues(dark.border),
     );
-    darkRef.current?.style.setProperty(
-      "--input",
-      extractHSLValues(Color("black").mix(Color("white"), 0.15).rgb().string()),
-    );
+    darkRef.current?.style.setProperty("--input", extractHSLValues(dark.input));
+    darkRef.current?.style.setProperty("--ring", extractHSLValues(dark.ring));
+  }, [hex, dark, light]);
 
-    darkRef.current?.style.setProperty("--ring", extractHSLValues(hex));
-  }, [hex, darkColors, lightColors]);
-
-  const lightCssPalette = `
-    --background: ${extractHSLValues(lightColors[lightColors.length - 1])};
-    --foreground: ${extractHSLValues(
-      Color(hex).mix(Color("black"), 0.9).hsl().string(),
-    )};
-
-    --primary: ${extractHSLValues(hex)};
-    --primary-foreground: ${extractHSLValues(Color("#ffffff").hex())};
-
-    --card: ${extractHSLValues(lightColors[lightColors.length - 1])};
-    --card-foreground: ${extractHSLValues(
-      Color(hex).mix(Color("black"), 0.9).hsl().string(),
-    )};
-
-    --popover: ${extractHSLValues(Color("#ffffff").hsl().hex())};
-    --popover-foreground: ${extractHSLValues(
-      Color(hex).mix(Color("black"), 0.9).hsl().string(),
-    )};
-
-    --secondary: ${extractHSLValues(
-      Color(hex).mix(Color("white"), 0.85).rgb().string(),
-    )};
-    --secondary-foreground: ${extractHSLValues(darkColors[0])};
-
-    --muted: ${extractHSLValues(
-      Color(hex).mix(Color("white"), 0.95).rgb().string(),
-    )};
-    --muted-foreground: ${extractHSLValues(
-      Color("#ffffff").hsl().darken(0.5).hex(),
-    )};
- 
-    --accent: ${extractHSLValues(
-      Color(hex).mix(Color("white"), 0.95).rgb().string(),
-    )};
-    --accent-foreground: ${extractHSLValues(hex)};
-
-    --destructive: 0 84.2% 60.2%;
-    --destructive-foreground: 210 40% 98%;
-
-    --border: ${extractHSLValues(Color("#ffffff").hsl().darken(0.1).hex())};
-    --input: ${extractHSLValues(Color("#ffffff").hsl().darken(0.1).hex())};
-    --ring: ${extractHSLValues(hex)};
-
-    --radius: 0.5rem;
-  `;
-  const darkCssPalette = `
-    --background: ${extractHSLValues(darkColors[0])};
-    --foreground: ${extractHSLValues(
-      Color(hex).mix(Color("white"), 0.85).hsl().string(),
-    )};
-
-    --primary: ${extractHSLValues(hex)};
-    --primary-foreground: ${extractHSLValues(Color("#ffffff").hex())};
-
-    --card: ${extractHSLValues(darkColors[0])};
-    --card-foreground: ${extractHSLValues(
-      Color(hex).mix(Color("white"), 0.85).hsl().string(),
-    )};
-
-    --popover: ${extractHSLValues(Color(darkColors[0]).hsl().hex())};
-    --popover-foreground: ${extractHSLValues(
-      Color(hex).mix(Color("white"), 0.85).hsl().string(),
-    )};
-
-    --secondary: ${extractHSLValues(
-      Color("black").mix(Color(hex), 0.35).rgb().string(),
-    )};
-    --secondary-foreground: ${extractHSLValues(Color("#ffffff").hex())};
-
-    --muted: ${extractHSLValues(
-      Color(darkColors[0]).mix(Color("white"), 0.1).rgb().string(),
-    )};
-    --muted-foreground: ${extractHSLValues(
-      Color(darkColors[0]).mix(Color("white"), 0.3).rgb().string(),
-    )};
- 
-    --accent: ${extractHSLValues(
-      Color("black").mix(Color(hex), 0.35).rgb().string(),
-    )};
-    --accent-foreground: ${extractHSLValues(
-      Color(hex).mix(Color("white"), 0.85).hsl().string(),
-    )};
-
-    --destructive: 0 84.2% 60.2%;
-    --destructive-foreground: 210 40% 98%;
-
-    --border: ${extractHSLValues(
-      Color("black").mix(Color("white"), 0.15).rgb().string(),
-    )};
-    --input: ${extractHSLValues(
-      Color("black").mix(Color("white"), 0.15).rgb().string(),
-    )};
-    --ring: ${extractHSLValues(hex)};
-
-    --radius: 0.5rem;
-  `;
+  const lightCssPalette = generateShadcnCssVariables(light);
+  const darkCssPalette = generateShadcnCssVariables(dark);
 
   return (
     <>
-      <div className="flex ">
+      <div className="flex space-x-4">
         <div>
           <h2 className="text-xl">Light variables</h2>
-          <SyntaxHighlighter style={oneLight} language="css">
+          <SyntaxHighlighter
+            style={{ "hljs-number": { color: "black" } }}
+            customStyle={{
+              background: "white",
+              color: "black",
+
+              borderRadius: "8px",
+              padding: "24px",
+            }}
+            language="css"
+          >
             {lightCssPalette}
           </SyntaxHighlighter>
         </div>
         <div>
           <h2 className="text-xl">Dark variables</h2>
-          <SyntaxHighlighter style={oneDark} language="css">
+          <SyntaxHighlighter
+            style={{ "hljs-number": { color: "white" } }}
+            customStyle={{
+              background: "black",
+              color: "white",
+              padding: "24px",
+            }}
+            language="css"
+          >
             {darkCssPalette}
           </SyntaxHighlighter>
         </div>
---
+++ src/utils/generateShadcnColorAttributes.ts
@@ -0,0 +1,52 @@
+import Color from "color";
+
+export function generateShadcnColorAttributes({
+  hex,
+  darkColors,
+  lightColors,
+}: {
+  hex: string;
+  darkColors: string[];
+  lightColors: string[];
+}) {
+  return {
+    light: {
+      background: lightColors[lightColors.length - 1],
+      foreground: Color(hex).mix(Color("black"), 0.9).hex(),
+      primary: hex,
+      primaryForeground: Color("white").hex(),
+      card: lightColors[lightColors.length - 1],
+      cardForeground: Color(hex).mix(Color("black"), 0.9).hex(),
+      popover: Color("white").hex(),
+      popoverForeground: Color(hex).mix(Color("black"), 0.9).hex(),
+      secondary: Color(hex).mix(Color("white"), 0.85).hex(),
+      secondaryForeground: darkColors[0],
+      muted: Color(hex).mix(Color("white"), 0.95).hex(),
+      mutedForeground: Color("white").hsl().darken(0.5).hex(),
+      accent: Color(hex).mix(Color("white"), 0.95).hex(),
+      accentForeground: hex,
+      border: Color("white").hsl().darken(0.1).hex(),
+      input: Color("white").hsl().darken(0.1).hex(),
+      ring: hex,
+    },
+    dark: {
+      background: darkColors[0],
+      foreground: Color(hex).mix(Color("white"), 0.85).hex(),
+      primary: hex,
+      primaryForeground: Color("white").hex(),
+      card: darkColors[0],
+      cardForeground: Color(hex).mix(Color("white"), 0.85).hex(),
+      popover: darkColors[0],
+      popoverForeground: Color(hex).mix(Color("white"), 0.85).hex(),
+      secondary: Color("black").mix(Color(hex), 0.35).hex(),
+      secondaryForeground: Color("white").hex(),
+      muted: Color(darkColors[0]).mix(Color("white"), 0.1).hex(),
+      mutedForeground: Color(darkColors[0]).mix(Color("white"), 0.3).hex(),
+      accent: Color("black").mix(Color(hex), 0.35).hex(),
+      accentForeground: Color(hex).mix(Color("white"), 0.85).hex(),
+      border: Color("black").mix(Color("white"), 0.15).hex(),
+      input: Color("black").mix(Color("white"), 0.15).hex(),
+      ring: hex,
+    },
+  };
+}

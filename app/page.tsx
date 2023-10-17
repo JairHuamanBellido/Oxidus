734d17c0b8a65e578e8043d06ad1c7bdc9323052
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2023-10-17 11:32:56 -0500

    feat(theme): update layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { ThemeContextProvider } from "@/src/contexts/ThemeContext/ThemeContext";
+import {
+  ThemeContextProvider,
+  useThemeContext,
+} from "@/src/contexts/ThemeContext/ThemeContext";
 
 import Sidebar from "../src/components/sidebar/Sidebar";
 import Header from "../src/components/header";
@@ -11,7 +14,36 @@ import { Button } from "@/src/components/shadcn/button";
 import TypographyMuted from "../src/components/typography/muted";
 import Link from "next/link";
 import ThemeVariablesSettingSidebar from "../src/components/sidebar/ThemeVariablesSettingSidebar";
+import Color from "color";
+import { ReactNode, useEffect, useState } from "react";
 
+const Layout = ({ children }: { children: ReactNode }) => {
+  const {
+    hex,
+    cssVariables: {
+      shadcn: { light, dark },
+    },
+    mode,
+  } = useThemeContext();
+
+  const [bg, setBg] = useState<string>("");
+
+  const background =
+    mode === "light" ? light.background.color : dark.background.color;
+  const mixColor = mode === "light" ? "black" : "white";
+  const backgroundVariant = Color(Color(background).hex())
+    .mix(Color(mixColor), 0.05)
+    .hex();
+
+  return (
+    <div
+      style={{ background: backgroundVariant }}
+      className="flex w-full h-[calc(100vh-80px)] relative p-8"
+    >
+      {children}
+    </div>
+  );
+};
 export default function Home() {
   return (
     <ThemeContextProvider>
@@ -20,28 +52,30 @@ export default function Home() {
         <Sidebar />
         <div className="w-[calc(100vw-600px)] h-screen relative">
           <Header />
-          <div className="flex w-full h-[calc(100vh-80px)] items-center justify-center">
-            <div className="w-[600px] space-y-4">
-              <div className="space-y-1 ">
-                <Label>Username</Label>
-                <Input placeholder="Type your username" />
-              </div>
-              <div className="space-y-1 ">
-                <Label>Password</Label>
-                <Input placeholder="Type your password" type="password" />
-              </div>
-              <div className="flex flex-col space-y-2">
-                <Button>Sign In</Button>
-                <Button variant={"secondary"}>Secondary</Button>
-                <TypographyMuted>
-                  Don't have an account?{" "}
-                  <Button asChild variant={"link"}>
-                    <Link href={"/"}> Sign Up</Link>
-                  </Button>
-                </TypographyMuted>{" "}
+          <Layout>
+            <div className="flex items-center justify-center bg-background rounded w-full h-full">
+              <div className="w-[600px] space-y-4">
+                <div className="space-y-1 ">
+                  <Label>Username</Label>
+                  <Input placeholder="Type your username" />
+                </div>
+                <div className="space-y-1 ">
+                  <Label>Password</Label>
+                  <Input placeholder="Type your password" type="password" />
+                </div>
+                <div className="flex flex-col space-y-2">
+                  <Button>Sign In</Button>
+                  <Button variant={"secondary"}>Secondary</Button>
+                  <TypographyMuted>
+                    Don't have an account?{" "}
+                    <Button asChild variant={"link"}>
+                      <Link href={"/"}> Sign Up</Link>
+                    </Button>
+                  </TypographyMuted>{" "}
+                </div>
               </div>
             </div>
-          </div>
+          </Layout>
         </div>
         <ThemeVariablesSettingSidebar />
       </main>
--- src/components/theme/ThemeImport.tsx
+++ src/components/theme/ThemeImport.tsx
@@ -1,42 +1,28 @@
-import { ChangeEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent } from "react";
 import { Input } from "../shadcn/input";
 import { Label } from "../shadcn/label";
 import { useThemeContext } from "@/src/contexts/ThemeContext/ThemeContext";
 import { Button } from "../shadcn/button";
+import useThemeImport from "@/src/hooks/useThemeImport";
 
 export default function ThemeImport() {
-  const { setTheme } = useThemeContext();
+  const { submit } = useThemeImport();
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files ? e.target.files[0] : null;
-    if (file) {
-      const reader = new FileReader();
-
-      reader.onload = () => {
-        try {
-          const json = JSON.parse(reader.result as string);
-          setTheme(json);
-        } catch (error) {
-          alert("Invalid format!. Check your JSON fiLE");
-        } finally {
-          e.target.value = "";
-        }
-      };
-
-      reader.readAsText(file, "UTF-8");
-    }
+    submit(file, e);
   };
   return (
-    <div className="flex items-center text-center">
+    <div className="flex items-center">
       <Button variant={"outline"}>
-        <Label htmlFor="theme-file">Submit file</Label>
+        <Label htmlFor="theme-file">Import theme</Label>
       </Button>
       <Input
         onChange={onChange}
         id="theme-file"
         type="file"
-        className="invisible"
-        placeholder="Submit file"
+        className="invisible w-0 h-0 p-0 m-0"
+        placeholder="Import theme"
       />
     </div>
   );

a08ddeed295a2780d0764901303d0f854bad0828
ruru <142723369+ruru-m07@users.noreply.github.com>
2024-04-21 11:40:46 +0530

    feat: ThemeToggle button
    close #36
    
    Co-Authored-By: ruru <ruru.dev07@gmail.com>

--- src/components/header/ThemeToggle.tsx
+++ src/components/header/ThemeToggle.tsx
@@ -1,56 +1,43 @@
 import { useThemeContext } from "@/src/contexts/ThemeContext/ThemeContext";
-import { Button } from "@/src/components/shadcn/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/src/components/shadcn/dropdown-menu";
+import { buttonVariants } from "@/src/components/shadcn/button";
 import { cn } from "@/src/utils/utils";
 import { MoonIcon, SunIcon } from "lucide-react";
+import { Switch } from "../shadcn/switch";
 
 export default function ThemeToggle() {
   const { setMode, mode } = useThemeContext();
 
+  const handleThemeChange = (checked: boolean) => {
+    checked ? setMode("dark") : setMode("light");
+  };
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="w-9 px-0">
-          <SunIcon
-            className={cn(
-              "h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all ",
-              { "-rotate-90": mode === "dark" },
-              { "scale-0": mode === "dark" },
-            )}
-          />
-          <MoonIcon
-            className={cn(
-              "absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100",
-              { "rotate-0": mode === "dark" },
-              { "scale-100": mode === "dark" },
-            )}
-          />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem
-          onClick={() => {
-            setMode("light");
-            (window as any).gtag("event", "light-mode");
-          }}
-        >
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={() => {
-            setMode("dark");
-            (window as any).gtag("event", "dark-mode");
-          }}
-        >
-          Dark
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div>
+      <div
+        className={cn(
+          buttonVariants({
+            variant: "ghost",
+            className: "w-9 px-0",
+          }),
+        )}
+      >
+        <SunIcon
+          className={cn(
+            "h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all ",
+            { "-rotate-90": mode === "dark" },
+            { "scale-0": mode === "dark" },
+          )}
+        />
+        <MoonIcon
+          className={cn(
+            "absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100",
+            { "rotate-0": mode === "dark" },
+            { "scale-100": mode === "dark" },
+          )}
+        />
+        <span className="sr-only">Toggle theme</span>
+      </div>
+      <Switch onCheckedChange={(e) => handleThemeChange(e)} />
+    </div>
   );
 }

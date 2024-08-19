2793de3554eacc2807018a97b4c794db5b4534aa
Pedro González Carretero <pedrogonzalezcarr@gmail.com>
2024-08-19 17:49:16 -0300

    Show toast after succesfully copying the code

--- src/components/header/CopyCodeButton.tsx
+++ src/components/header/CopyCodeButton.tsx
@@ -8,16 +8,21 @@ import {
   Tooltip,
 } from "../shadcn/tooltip";
 import { Button } from "../shadcn/button";
+import { useToast } from "@/src/components/shadcn/use-toast";
 
 interface CopyButtonProps {
   codeToCopy: string;
 }
 
 export default function CopyButton({ codeToCopy }: CopyButtonProps) {
+  const { toast } = useToast();
   const handleCopy = () => {
     navigator.clipboard.writeText(codeToCopy).then(
       () => {
         console.log("Text copied to clipboard");
+        toast({
+          title: "CSS variables copied to clipboard",
+        });
       },
       (err) => {
         console.error("Failed to copy text: ", err);
